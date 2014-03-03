439d6130b95fea316cac743519bef85f00763e5d
Andrew Jiang <andrew.h.jiang@gmail.com>
2014-03-02 23:40:41 -0500

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Babelmail::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -60,7 +60,7 @@ Babelmail::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w[*.png *.jp?g *.gif *.ico *.woff *.ttf] 
+  config.assets.precompile += %w[*.png *.jp?g *.gif *.ico] 
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
